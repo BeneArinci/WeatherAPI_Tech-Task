491e293e0cc8ba4b988c1809d6bc179154e454f2
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-30 12:30:30 +0000

    Installed fetch-node. Completed function for getting the max temperature for the year

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+const fetch = require('node-fetch');
+
+const headers = {
+  "x-api-key": "mcDLmlxrtw7ZHC70gD8FL4rtrXSPsUEB4iSp4lg3",
+}
 // Get maximum Temperature for a year - Must return a number
 exports.getMaxTemperature = async ({location, year}) => {
-	return 0;
+	let response = await fetch(`https://grudwxjpa2.execute-api.eu-west-2.amazonaws.com/dev/${location}/year/${year}`, {
+		method: "GET",
+		headers: headers
+	})
+	let data = await response.json()
+	let maxTemp = Math.max.apply(Math, data.result.map(function(temp) { return temp.temperature_max; }))
+	return maxTemp
 }
 
 // Get minimum temperature for a year - Must return a number
