1ca85da3a6164ada423c8ed60ea02979805f85b1
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-01 20:52:28 +0000

    Changed mock data for sun hours to make the test more relevant

--- tests/getAverageSunHours.test.js
+++ tests/getAverageSunHours.test.js
@@ -9,19 +9,19 @@ var nodeFetch = require('node-fetch')
 
 
 describe('getAverageSunHours using apiMock', () => {
-	it('Successfully gets the min Temperature based on mock data', async () =>{
+	it('Successfully gets the the average sun hours based on mock data', async () =>{
     const location = 'oxford';
 		const year = 2018;
     sinon.stub(nodeFetch, 'Promise').returns(Promise.resolve({ json: () => { return apiDataMock} }))
     await getMaxTemperature({location:location, year:year});
 		const result = await getAverageSunHours({location:location, year:year});
-		expect(result).toEqual(100.0)
+		expect(result).toEqual(75)
 		sinon.restore();
 	})
 })
 
 describe('getAverageSunHours', () => {
-	it('Successfully gets the min Temperature for oxford 2018', async () =>{
+	it('Successfully gets the average sun hours for oxford 2018', async () =>{
 		const location = 'oxford';
     const year = 2018;
     await getMaxTemperature({location:location, year:year});
