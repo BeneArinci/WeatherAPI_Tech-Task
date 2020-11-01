ecbbd0ad0accca7de59b4d09a5e633ff20986deb
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-01 23:53:43 +0000

    Added test for getAverageSunHoursForLocation - Heathrow

--- tests/getAverageSunHoursForLocation.test.js
+++ tests/getAverageSunHoursForLocation.test.js
@@ -1,12 +1,19 @@
-const {getAverageSunHoursForLocation, getMinTemperatureForLocation} = require('../src/index');
+const {getAverageSunHoursForLocation} = require('../src/index');
 const {getMaxTemperatureForLocation} = require('../src/index');
 
 describe('getAverageSunHoursForLocation', () => {
 	it('Successfully gets the overall average of sun hours for Oxford', async () =>{
-    jest.setTimeout(80000);
+    jest.setTimeout(60000);
     const location = 'oxford';
     await getMaxTemperatureForLocation({location})
 		const result = await getAverageSunHoursForLocation({location:location});
 		expect(result).toEqual(128.1);
+  })
+  it('Successfully gets the overall average of sun hours for Heathrow', async () =>{
+    jest.setTimeout(60000);
+    const location = 'heathrow';
+    await getMaxTemperatureForLocation({location})
+		const result = await getAverageSunHoursForLocation({location:location});
+		expect(result).toEqual(128.5);
 	})
 })
