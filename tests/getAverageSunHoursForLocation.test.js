36033611eda3ea5a1203357503732c61f982464a
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-02 11:37:11 +0000

    Fixed getAverageSunHoursForLocation test

--- tests/getAverageSunHoursForLocation.test.js
+++ tests/getAverageSunHoursForLocation.test.js
@@ -1,10 +1,13 @@
 const {getAverageSunHoursForLocation} = require('../src/index');
-const {getMaxTemperatureForLocation} = require('../src/index');
+const {checkDataAvailability} = require('../src/index');
+const {getMaxTemperatureForLocation} = require('../src/index')
 
 describe('getAverageSunHoursForLocation', () => {
 	it('Successfully gets the overall average of sun hours for Oxford', async () =>{
     jest.setTimeout(60000);
     const location = 'oxford';
+    const year = '2012'
+    await checkDataAvailability({location, year})
     await getMaxTemperatureForLocation({location})
 		const result = await getAverageSunHoursForLocation({location:location});
 		expect(result).toEqual(128.1);
@@ -12,6 +15,8 @@ describe('getAverageSunHoursForLocation', () => {
   it('Successfully gets the overall average of sun hours for Heathrow', async () =>{
     jest.setTimeout(60000);
     const location = 'heathrow';
+    const year = '2012'
+    await checkDataAvailability({location, year})
     await getMaxTemperatureForLocation({location})
 		const result = await getAverageSunHoursForLocation({location:location});
 		expect(result).toEqual(128.5);
