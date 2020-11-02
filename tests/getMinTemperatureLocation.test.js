567455e5bf7859c34c96ca3b79a82ebbc3d518c9
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-02 00:01:43 +0000

    Added test for getMinTemperatureForLocation, location heathrow

--- tests/getMinTemperatureLocation.test.js
+++ tests/getMinTemperatureLocation.test.js
@@ -2,11 +2,19 @@ const {getMinTemperatureForLocation} = require('../src/index');
 const {getMaxTemperatureForLocation} = require('../src/index');
 
 describe('getMinTemperatureForLocation', () => {
-	it('Successfully gets the overall min Temperature for Oxford', async () =>{
+  it('Successfully gets the overall min Temperature for Heathrow', async () =>{
+    jest.setTimeout(30000);
+    const location = 'heathrow';
+    await getMaxTemperatureForLocation({location:location})
+		const result = await getMinTemperatureForLocation({location:location});
+		expect(result).toEqual(-4.6);
+  })
+  
+  it('Successfully gets the overall min Temperature for Oxford', async () =>{
     jest.setTimeout(30000);
     const location = 'oxford';
     await getMaxTemperatureForLocation({location:location})
 		const result = await getMinTemperatureForLocation({location:location});
 		expect(result).toEqual(-5.8);
-	})
+  })
 })
